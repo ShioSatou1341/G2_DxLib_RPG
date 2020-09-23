3c42d74004be749e25b5007beab32744b52b43f5
Osatou33-4 <hj-g19003@sist.ac.jp>
2020-09-23 10:01:55 +0900

    mapの追加

---
+++ IMAGE/MAP/遺跡.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="遺跡" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../../Users/g19003/Documents/マップ素材/遺跡.png" width="512" height="512"/>
+</tileset>
